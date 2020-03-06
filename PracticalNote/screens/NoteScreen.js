e9269fdc01526465be3847b26285f8d34427b2a2
Daeseong7966 <41766637+Daeseong7966@users.noreply.github.com>
2020-03-06 23:16:04 +0900

    [Add]
    Add screens

---
+++ PracticalNote/screens/NoteScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class NoteScreen extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>NoteScreen</Text>
+            </View>
+        );
+    }
+}
+
+export default NoteScreen;
---
+++ PracticalNote/screens/SettingScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class SettingScreen extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>SettingScreen</Text>
+            </View>
+        );
+    }
+}
+
+export default SettingScreen;
